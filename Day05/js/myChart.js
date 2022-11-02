08c43b6e100a2e31aa3703331ce13efe4220e2b9
DavidJo84 <112602671+DavidJo84@users.noreply.github.com>
2022-11-02 16:05:30 +0900

    add file

--- Day05/js/myChart.js
+++ Day05/js/myChart.js
@@ -22,7 +22,7 @@
         '토'
       ],
       datasets: [{
-        label:'환자수',
+        label: '환자수',
         data: [
           42,
           45,
@@ -70,14 +70,14 @@
         '양방과'
       ],
       datasets: [{
-        label : '# of Votes',
+        label: '# of Votes',
         data: [
           20,
           10,
           8
         ],
-        backgroundColor:[
-          '#905E96','#D58BDD','#FF99D7'
+        backgroundColor: [
+          '#905E96', '#D58BDD', '#FF99D7'
         ],
         lineTension: 0,
         borderColor: 'black',
@@ -100,5 +100,39 @@
   })
 })()
 
-var a = document.getElementById(headline);
-a.innerHTML="되는건가?";
+// var arr = ['11/02 14시 4층에서 전체 회의 있습니다.', '12/14 18시 송년회있습니다.'
+//   , '11/05 19시 병동 회식있습니다.'];
+  
+//   document.getElementById('headline').innerText =arr[0]
+//   let count = 1;
+//   setInterval(()=>{
+//   if(count<3){
+//     document.getElementById('headline').innerText =arr[count++]
+//   }else{
+//     count=0;
+//   }},3000);
+
+  document.addEventListener('DOMContentLoaded', ()=>{
+    var interval = window.setInterval(rollingCallback, 3000);
+})
+function rollingCallback(){
+    //.prev 클래스 삭제
+    document.querySelector('.rollingbanner .prev').classList.remove('prev');
+
+    //.current -> .prev
+    let current = document.querySelector('.rollingbanner .current');
+    current.classList.remove('current');
+    current.classList.add('prev');
+
+    //.next -> .current
+    let next = document.querySelector('.rollingbanner .next');
+    //다음 목록 요소가 널인지 체크
+    if(next.nextElementSibling == null){
+        document.querySelector('.rollingbanner ul li:first-child').classList.add('next');
+    }else{
+        //목록 처음 요소를 다음 요소로 선택
+        next.nextElementSibling.classList.add('next');
+    }
+    next.classList.remove('next');
+    next.classList.add('current');
+}
